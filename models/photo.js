72556b382feedd01209ee7a09b12dee51021305d
afrivero <alvaro18195@gmail.com>
2017-06-15 18:16:16 +0200

    Create photo.js

--- models/photo.js
+++ models/photo.js
@@ -9,27 +9,8 @@ photo_id: {
 
 */
 
-var photos_url = process.env.PHOTOS_URL || "http://localhost:8000"
+var photos_url = process.env.PHOTOS_URL || "http://10.1.3.1:8000"
 
 exports.photos = {
-	1: {
-		name: 'Wood',
-		url: photos_url + '/photos/photo1.jpg'
-	},
-	2: {
-		name: 'Desktop',
-		url: photos_url + '/photos/photo2.jpg'
-	},
-	3: {
-		name: 'Man',
-		url: photos_url + '/photos/photo3.jpg'
-	},
-	4: {
-		name: 'Woman',
-		url: photos_url + '/photos/photo4.jpg'
-	},
-	5: {
-		name: 'People',
-		url: photos_url + '/photos/photo5.jpg'
-	}
+
 };
